01213830e8f8fbd65e18365ef188c020bcb0db44
christi <40750751+itschristi@users.noreply.github.com>
2019-02-28 13:03:08 -0600

    Add files via upload

---
+++ CitationTool/src/Citation.java
@@ -0,0 +1,21 @@
+
+public class Citation implements CitationAPI {
+	public static void main(String[] args) {
+		// 
+	}
+	
+	public String getAuthor() {
+		
+	}
+	public String getAccessedDate() { 
+		
+	}
+	public String getPublishedDate() {
+		
+	}
+	public String getURL() {
+		
+	}
+	
+	public String sort();
+}
---
+++ CitationTool/src/CitationAPI.java
@@ -0,0 +1,9 @@
+
+public interface CitationAPI {
+	public void MLA();
+	public void APA();
+	public String getAuthor();
+	public String getAccessedDate();
+	public String getPublishedDate();
+	public String getURL();
+}
