5402102e39e0278b9a3c66584612c7ecc175d653
christi <40750751+itschristi@users.noreply.github.com>
2019-03-01 20:12:53 -0600

    Update MLA.java

--- WebCitationTools/MLA.java
+++ WebCitationTools/MLA.java
@@ -1,38 +1,60 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MLA extends Citation implements CitationAPI {
-	private static ArrayList<Citation> mla;
+public class MLA extends Citation implements CitationAPI  {
+	private static ArrayList<Citation> storedMLA;
+	private static ArrayList<Citation> sortedMLA;
 	
 	public MLA() {
-		mla = new ArrayList<>();
+		storedMLA = new ArrayList<>();
+		sortedMLA = new ArrayList<>();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NullPointerException {
 		Scanner keyboard = new Scanner(System.in);
 		int index = 0;
 		while(true) {
-			System.out.print("Create new MLA citation? (Y/N)");
+			System.out.print("Create new MLA citation? (Y / N) : ");
 			String input = keyboard.nextLine();
 			input = input.toUpperCase();
 			
-			if(input.equals("MLA")) {
-				mla.add(new Citation());
-			}	
+			if(input.equals("Y")) {
+				// Exception in thread "main" 
+				// java.lang.NullPointerException
+				storedMLA.add(new Citation());
+			}
+			
+			System.out.println(storedMLA.get(index).toString());
+			addCitation(storedMLA.get(index));
+
+			System.out.println("\nCreate another MLA citation? (Y / N) :");
+			String input2 = keyboard.nextLine();
+			input2 = input2.toUpperCase();
+			
+			if(input2.equals("N")) {
+				System.out.println("Processing Citations...\n\n");
+				
+				for(int i = 0; i < sortedMLA.size(); i++) {
+					System.out.println(sortedMLA.get(i).toString());
+					System.out.println("");
+				}
+				
+				break;
+			}
 		}
 	}
 	
-	public void addCitation(Citation c) {
-		if (mla.isEmpty()) {
-			mla.add(c);
+	public static void addCitation(Citation c) {
+		if (sortedMLA.isEmpty()) {
+			sortedMLA.add(c);
 		} else {
-			for (int i = 0; i < mla.size(); i++) {
-				if (mla.get(i).compareTo(c) <= 0) {
-					mla.add(i, c);
+			for (int i = 0; i < sortedMLA.size(); i++) {
+				if (sortedMLA.get(i).compareTo(c) <= 0) {
+					sortedMLA.add(i, c);
 					return;
 				}
 			}
-			mla.add(c);	
+			sortedMLA.add(c);	
 		}
 	}
 	
